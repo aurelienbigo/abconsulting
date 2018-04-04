f847c5e04dce983ab4109b8ad9b922f63a9fb56a
aurelien bigo <aurelien.bigo@gmail.com>
2018-04-04 19:01:53 +0200

    contact page amelio et texte

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+
+  get 'thanks', to: 'pages#thanks'
   get 'contact', to: 'pages#contact'
   get 'service', to: 'pages#service'
   get 'realisation', to: 'pages#realisation'
   get 'projet', to: 'pages#projet'
   get 'about', to: 'pages#about'
+  get 'mentions', to: 'pages#mentions'
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
